f6f39daaaf53e7c0a3b96c9073885afab7894441
lava <lavakumared15@gmail.com>
2022-03-24 12:07:47 +0530

    buttons toggle without bg change

--- src/CoinBurnHistoryOrderCard.jsx
+++ src/CoinBurnHistoryOrderCard.jsx
@@ -2,14 +2,14 @@ import './css/coinHistoryOrderCard.css'
 import pic from './images/fire.png';
 import calenderPic from './images/calender.png';
 import coinPic from './images/500coin.jpg';
-export default function CoinHistoryOrderCard() {
+export default function CoinBurnHistoryOrderCard() {
     return (
         <>
             <div className='orderCardContainer'>
                 <img className='fire' src={pic} alt="" />
                 <div className='historyContentCard'>
                     <div className='historyContent'>
-                        Burnt 500 coins on Order Number #123494
+                        Rewarded 500 coins on Order Number #123494
                     </div>
                     <div className='historyInfo'>
                         <img src={calenderPic} className='calender' alt="" />
--- src/RewardsAndBurns.jsx
+++ src/RewardsAndBurns.jsx
@@ -1,7 +1,28 @@
 import CoinHistoryOrderCard from './CoinHistoryOrderCard';
+import CoinBurnHistoryOrderCard from './CoinBurnHistoryOrderCard';
 import './css/RewardsAndBurns.css'
 import './css/coinHistoryCard.css'
+import { useState } from 'react';
 const RewardsAndBurns = () => {
+    const [trackHistory,setTrackHistory] = useState(false)
+    const [buttonsState,setButtonState] = useState({
+        rewardsButtonColor: "white",
+        burnsButtonColor:"none"
+    })
+    const showRewards = () =>{
+        setTrackHistory(false);
+        setButtonState({
+            rewardsButtonColor: "white",
+            burnsButtonColor:"none"
+        })
+    }
+    const showBurns = () => {
+        setTrackHistory(true);
+        setButtonState({
+            rewardsButtonColor: "none",
+            burnsButtonColor:"white"
+        })
+    }
   return (
     <>
         <div className='rewardsAndBurnsContainer'>
@@ -9,18 +30,38 @@ const RewardsAndBurns = () => {
                 History
             </div>
             <div className='toggleButtons'>
-                <button className='rewards'>
+                <button className='rewards' onClick={showRewards}> 
                     Rewards
                 </button>
-                <button className='burns'>
+                <button className='burns' onClick={showBurns}>
                     Burns
                 </button>
             </div>  
             <div className='coinsOrderCard' style={{marginTop:"25px"}}>
-                <CoinHistoryOrderCard />
-                <CoinHistoryOrderCard />
-                <CoinHistoryOrderCard />
-                <CoinHistoryOrderCard />
+                {
+                trackHistory ? <CoinHistoryOrderCard /> : null
+                }
+                {
+                trackHistory ? <CoinHistoryOrderCard /> : null
+                }
+                {
+                trackHistory ? <CoinHistoryOrderCard /> : null
+                }
+                {
+                trackHistory ? <CoinHistoryOrderCard /> : null
+                }
+                {
+                !trackHistory? <CoinBurnHistoryOrderCard/> :  null
+                }
+                {
+                !trackHistory? <CoinBurnHistoryOrderCard/> :  null
+                }
+                {
+                !trackHistory? <CoinBurnHistoryOrderCard/> :  null
+                }
+                {
+                !trackHistory? <CoinBurnHistoryOrderCard/> :  null
+                }
             </div>
         </div>
     </>
